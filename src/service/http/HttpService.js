480fda7750b12e21456d4024e8951f8720d0c2d3
Syed Mohamad Arif <syedarifjr@gmail.com>
2024-04-13 00:54:31 +0800

    port update

--- src/service/http/HttpService.js
+++ src/service/http/HttpService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {API_BASE_URL} from 'react-native-dotenv';
 
 const httpService = axios.create({
-  baseURL: 'http://localhost:3306/to-do/v1/',
+  baseURL: 'http://localhost:3000/to-do/v1/',
   //   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
